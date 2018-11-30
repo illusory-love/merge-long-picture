7c5ca00e7794d97e3d9aa776d10082c7da4c0393
wugang <wugang@yit.com>
2018-11-30 18:07:09 +0800

    save

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import merge from './libs/index'
+// import merge from './libs/index'
 
-merge()
+// merge()
+
+import program from 'commander'
+
+import {version} from './package.json'
+
+program
+    .version(`v${version}`)
+    .option('-d --dir <directory>', '图片目录')
+    .option('-i --img <images>', '指定一组图片(>= 1)')
+    .parse(process.argv)
